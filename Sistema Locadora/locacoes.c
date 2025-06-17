c9310f9b9a62575341225d9763de21f78fb24076
dudamssr <eduarda.massarosantos@gmail.com>
2025-06-17 11:09:38 -0300

    Update locacoes.c

--- Sistema Locadora/locacoes.c
+++ Sistema Locadora/locacoes.c
@@ -15,18 +15,31 @@ void realizarLocacao(Locacao *locacoes, int *totalLocacoes, Cliente *clientes, i
     printf("5 - Sair\n");
     scanf("%d", &genero);
 }
+void realizarLocacao(Locacao *locacoes, int *totalLocacoes, Cliente *clientes, int totalClientes) {
+    int genero;
+    printf("\n ---== Realizar Locação ==---\n");
+    printf("Escolha o genero desejado: \n");
+    printf("1 - Romance\n");
+    printf("2 - Terror ou Suspense\n");
+    printf("3 - Comédia\n");
+    printf("4 - Drama\n");
+    printf("5 - Sair\n");
+    scanf("%d", &genero);
+    
+    printf("Digite o ID do cliente: ");
+    scanf("%d", &locacoes[*totalLocacoes].IDCliente);
+
+    printf("Digite o nome do filme: ");
+    scanf("%s", locacoes[*totalLocacoes].Filme);
+
+    (*totalLocacoes)++;
+    printf("Locação realizada com sucesso!\n");
+}
+
 void listarLocacoes(Locacao *locacoes, int totalLocacoes) {
     printf("\n ---== Listar Locações ==---\n");
     for (int i = 0; i < totalLocacoes; i++) {
         printf("ID Cliente: %d\n", locacoes[i].IDCliente);
         printf("Filme: %s\n", locacoes[i].Filme);
-    }
-}
-void buscarLocacoesPorCliente(Locacao *locacoes, int totalLocacoes, int idCliente) {
-    printf("\n---== Buscar Locações por Cliente ==---\n");
-    for (int i = 0; i < totalLocacoes; i++) {
-        if (locacoes[i].IDCliente == idCliente) {
-            printf("Filme: %s\n", locacoes[i].Filme);
-        }
     }
 }
